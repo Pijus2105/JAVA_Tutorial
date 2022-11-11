07284533678bfe6048d8751b9742b04edbdab361
Elphill <Elphill@DESKTOP-QEBQ8BA>
2022-11-11 15:45:33 +0530

    4th

--- src/OOPs/Abstraction/classname.java
+++ src/OOPs/Abstraction/classname.java
@@ -9,16 +9,16 @@ public class classname {
 
 		public static void main(String[] args) {
 			
-//		
-//			 String str = "Mandate of the Fisheries department is to increase fish production by optimum utilization of all water bodies, to create infrastructure for post harvest management, to develop an enabling eco-system for adoption of scientific pisciculture, to strive for socio- economic uplift of the fishermen at large and to ensure availability of fish at affordable cost to the masses.";
+		
+//			 String str = "Maate of the Fisheries department is to increase fish production by optimum utilization of all water bodies, to create infrastructure for post harvest management, to develop an enabling eco-system for adoption of scientific pisciculture, to strive for socio- economic uplift of the fishermen at large and to ensure availability of fish at affordable cost to the masses.";
 //			    String h = str.replaceAll("\\s+"," ").trim();
 //			    
 //			    if(h.matches(str)) {
 //			    	System.out.println("Single Space");
-//			    } else {
+//		    } else {
 //			    	System.out.println("More Space");
 //			    }
-//		        
+		        
 			
 			
 			
