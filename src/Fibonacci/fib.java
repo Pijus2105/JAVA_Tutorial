a45bb28df145c70ab9c3c7d46b62b0ac888cbd42
Elphill <Elphill@DESKTOP-QEBQ8BA>
2022-12-06 13:01:34 +0530

    commit

---
+++ src/Fibonacci/fib.java
@@ -0,0 +1,18 @@
+package Fibonacci;
+
+public class fib {
+
+	int number  = 10;
+	public int fib() {
+		if (number<=1) {
+			
+		} else {
+			
+		}
+		return number;
+		
+		
+	}
+	
+	
+}
---
+++ src/Fibonacci/fibmain.java
@@ -0,0 +1,13 @@
+package Fibonacci;
+
+public class fibmain {
+
+	public static void main(String[] args) {
+		 fib fb = new  fib();
+		 int fibonacci = fb.fib();
+		 
+		 System.out.println(fibonacci);
+
+	}
+
+}
